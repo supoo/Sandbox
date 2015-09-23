600664c6f9b90129158b01ca99f53b4ca7384062
Susannah Poon <susannah.poon@resaas.com>
2015-09-23 12:45:22 -0700

    New .NET project to show URLs

---
+++ Sandbox/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sandbox.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+
+			var s1a = System.Web.HttpContext.Current.Request.ServerVariables["SERVER_NAME"];
+			var s1b = System.Web.HttpContext.Current.Request.ServerVariables["SERVER_PORT"];
+
+			var s2a = this.HttpContext.Request.Url.Host;
+			var s2b = this.HttpContext.Request.Url.Authority;
+			var s2c = this.HttpContext.Request.Url.Scheme;
+
+			ViewBag.Message = string.Format("ServerVariables: {0} | {1} || Request: {2} | {3} | {4}", s1a, s1b, s2a, s2b, s2c);
+
+			return View();
+		}
+
+		public ActionResult About()
+		{
+			ViewBag.Message = "Your app description page.";
+
+			return View();
+		}
+
+		public ActionResult Contact()
+		{
+			ViewBag.Message = "Your contact page.";
+
+			return View();
+		}
+	}
+}
